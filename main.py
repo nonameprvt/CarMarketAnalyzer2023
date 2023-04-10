f8f12508483e56d430fc532f33bfbad006bc4276
Ivan Melnikov <noname_mlnckv@icloud.com>
2023-04-11 01:25:09 +0300

    Исправлены ошибки. Теперь парсинг идет по машинам Москвы и Московской Области, а также рассматривает все бренды по отдельности.

--- main.py
+++ main.py
@@ -9,84 +9,115 @@ data = pd.DataFrame(
      'Description': [], 'Link': []})
 
 browser = webdriver.Chrome()
-browser.get("https://www.avito.ru/all/avtomobili?cd=1")
+browser.get("https://www.avito.ru/moskva_i_mo/avtomobili")
 
-for page in range(1, 6):
-    time.sleep(2)
+all_cars_button = browser.find_element(
+    By.XPATH, "//button[contains(@class, 'popular-rubricator-button')]")
 
-    name = browser.find_elements(By.XPATH,
-                                 "//h3[contains(@itemprop, 'name')]")
-    # достаем -> .text
-    price = browser.find_elements(By.XPATH,
-                                  "//meta[contains(@itemprop, 'price')]")
-    # достаем четные элементы, там цена. В нечетных RUB
-    params = browser.find_elements(By.XPATH,
-                                   "//div[contains(@data-marker, "
-                                   "'item-specific-params')]")
-    # достаем -> .text.split(', '), получаем массив характеристик
-    description = browser.find_elements(By.CLASS_NAME,
-                                        "iva-item-descriptionStep-C0ty1")
-    # достаем -> .text
-    seller = browser.find_elements(By.XPATH,
-                                   "//div[contains(@data-marker, 'item-line')]")
-    # достаем -> .text
-    city = browser.find_elements(By.XPATH,
-                                 "//div[contains(@class, 'geo-address')]")
-    # достаем -> .text
-    link = browser.find_elements(By.XPATH,
-                                 "//a[contains(@data-marker, 'item-title')]")
-    # достаем -> .get_attribute('href')
-    time.sleep(2)
+all_cars_button.click()
 
-    for car in range(len(name)):
-        if len(params[car].text.split(', ')) > 4:
-            specs = params[car].text.split(', ')
-            mileage, engine, body, transmission, fuel = \
-                specs[0], specs[1], specs[2], specs[3], specs[4]
-        else:
-            mileage, engine, body, transmission, fuel = 'Ошибка', 'Ошибка', \
-                                                        'Ошибка', 'Ошибка', \
-                                                        'Ошибка'
-        if len(name) > car:
-            car_name = name[car].text
-        else:
-            car_name = 'Ошибка'
+models = browser.find_elements(
+    By.XPATH, "//a[contains(@class, 'popular-rubricator-link')]")
 
-        if len(price) > 2 * car + 1:
-            car_price = price[2 * car + 1].get_attribute('content')
-        else:
-            car_price = 'Ошибка'
+checker = 1
 
-        if len(seller) > car:
-            car_seller = seller[car].text
-        else:
-            car_seller = 'Ошибка'
+for model in models:
+    page_link = model.get_attribute('href')
 
-        if len(city) > car:
-            car_city = city[car].text
-        else:
-            car_city = 'Ошибка'
+    model_browser = webdriver.Chrome()
+    model_browser.get(page_link)
 
-        if len(description) > car:
-            car_description = description[car].text
-        else:
-            car_description = 'Ошибка'
+    while True:
+        check_no_results = False
+        try:
+            no_results_title = model_browser.find_elements(
+                By.XPATH, "//h2[contains(@class, 'no-results-title')]")
+        except:
+            check_no_results = True
 
-        if len(link) > car:
-            car_link = link[car].get_attribute('href')
-        else:
-            car_link = 'Ошибка'
+        if not check_no_results:
+            break
 
-        data.loc[len(data.index)] = [car_name, car_price, engine, mileage, body,
-                                     fuel, transmission, car_seller, car_city,
-                                     car_description, car_link]
+        time.sleep(2)
 
-    time.sleep(2)
-    next_page = \
-        browser.find_element(By.XPATH,
-                             "//span[contains(@data-marker, "
-                             "'pagination-button/next')]")
-    next_page.click()
+        name = model_browser.find_elements(By.XPATH,
+                                           "//h3[contains(@itemprop, 'name')]")
+        # достаем -> .text
+        price = model_browser.find_elements(
+            By.XPATH, "//meta[contains(@itemprop, 'price')]")
+        # достаем четные элементы, там цена. В нечетных RUB
+        params = model_browser.find_elements(
+            By.XPATH, "//div[contains(@data-marker, 'item-specific-params')]")
+        # достаем -> .text.split(', '), получаем массив характеристик
+        description = model_browser.find_elements(
+            By.CLASS_NAME, "iva-item-descriptionStep-C0ty1")
+        # достаем -> .text
+        seller = model_browser.find_elements(
+            By.XPATH, "//div[contains(@data-marker, 'item-line')]")
+        # достаем -> .text
+        city = model_browser.find_elements(
+            By.XPATH, "//div[contains(@class, 'geo-georeferences')]")
+        # достаем -> .text
+        link = model_browser.find_elements(
+            By.XPATH, "//a[contains(@data-marker, 'item-title')]")
+        # достаем -> .get_attribute('href')
+        time.sleep(2)
+
+        for car in range(len(name)):
+            if len(params[car].text.split(', ')) > 4:
+                specs = params[car].text.split(', ')
+                mileage, engine, body, transmission, fuel = \
+                    specs[0], specs[1], specs[2], specs[3], specs[4]
+            else:
+                mileage, engine, body, transmission, fuel = \
+                    'Ошибка', 'Ошибка', 'Ошибка', 'Ошибка', 'Ошибка'
+            if len(name) > car:
+                car_name = name[car].text
+            else:
+                car_name = 'Ошибка'
+
+            if len(price) > 2 * car + 1:
+                car_price = price[2 * car + 1].get_attribute('content')
+            else:
+                car_price = 'Ошибка'
+
+            if len(seller) > car:
+                car_seller = seller[car].text
+            else:
+                car_seller = 'Ошибка'
+
+            if len(city) > car:
+                car_city = city[car].text
+            else:
+                car_city = 'Ошибка'
+
+            if len(description) > car:
+                car_description = description[car].text
+            else:
+                car_description = 'Ошибка'
+
+            if len(link) > car:
+                car_link = link[car].get_attribute('href')
+            else:
+                car_link = 'Ошибка'
+
+            data.loc[len(data.index)] = [car_name, car_price, engine, mileage,
+                                         body,
+                                         fuel, transmission, car_seller,
+                                         car_city,
+                                         car_description, car_link]
+
+        time.sleep(2)
+        try:
+            next_page = model_browser.find_element(
+                By.XPATH, "//a[contains(@data-marker, "
+                          "'pagination-button/next')]")
+
+            next_page.click()
+        except:
+            break
+
+    model_browser.close()
 
 browser.close()
 data.to_csv('avito.csv')
